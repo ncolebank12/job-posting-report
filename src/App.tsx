a855e3034b72e309b92521f8b7bcf24883f3ca42
Nic Colebank <niccolebank@gmail.com>
2024-08-08 18:27:58 -0400

    changed to TS

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <div>
         <h1>Job Posting Report</h1>
         <h2>{count} other people have reported this listing as fraudalent.</h2>
       </div>
-
       <button onClick={() => setCount(count + 1)} />
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
