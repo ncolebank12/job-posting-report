f078c8248f402ac5a2bb8ef47b49f60e645df55a
Nic Colebank <niccolebank@gmail.com>
2024-10-14 16:08:09 -0700

    comments formatted but not scrollable

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,14 +1,19 @@
 import { JobPostData } from "../types";
-import styles from "../styles/Comments.module.css"
+import styles from "../styles/Comments.module.css";
+import AvatarIcon from "../assets/avatar-icon.png";
 
 const Comments = ({ postData }: { postData: JobPostData | undefined }) => {
     return (
         <div className={styles.container}>
+            <div className={styles.spacer} />
             {
                 postData?.comments ? postData.comments.map((comment, i) => {
                     return (
                         <div key={i} className={styles.comment}>
-                            <div className={styles.commentHeader}>{new Date(comment.timestamp.seconds * 1000).toDateString()}</div>
+                            <div className={styles.commentHeader}>
+                                <img className={styles.img} src={AvatarIcon}/>
+                                {new Date(comment.timestamp.seconds * 1000).toDateString()}
+                            </div>
                             <div className={styles.commentBody}>{comment.body}</div>
                         </div>
                     )
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 const Layout = () => {
     return (
-        <div style={{ width: "100vw" }}>
+        <div style={{ width: "100vw", maxHeight: "200px" }}>
             <Header title="Job Posting Report" />
             <Outlet />
         </div>
